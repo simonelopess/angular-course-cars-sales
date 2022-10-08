89e61f4c86e2081bbf40173bc8764b6f53c91fe0
Simone Lopes <sisilopes20@gmail.com>
2022-10-08 20:49:54 -0300

    add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{
   path: '', component: LoginComponent
-}];
+},
+{ path: 'dashboard', loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule) } //configuração lazyloading
+  //lazy loading permite carregar component quando acessado
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -2,6 +2,7 @@ import { AlertService } from './../../resources/services/alert.service';
 import { LoginService } from './../../resources/services/login.service';
 import { Component, OnInit } from '@angular/core';
 import { RequestLogin } from '../../resources/models/RequestLogin'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,7 @@ import { RequestLogin } from '../../resources/models/RequestLogin'
 export class LoginComponent implements OnInit {
   public requestLogin: RequestLogin = { login: '', password: '' };
 
-  constructor(private loginService: LoginService, private alertService: AlertService) { }
+  constructor(private loginService: LoginService, private alertService: AlertService, private router: Router) { }
 
   ngOnInit(): void {
     this.requestLogin = new RequestLogin();
@@ -19,7 +20,7 @@ export class LoginComponent implements OnInit {
 
   public doLogin(): void {
     this.loginService.doLogin(this.requestLogin).subscribe(data => {
-      this.alertService.info("Funcionalidade não implementada", "Login Realizado",);
+      this.router.navigate(['dashboard'])
     },
       httpError => {
         this.alertService.error("CPF ou Senha inválidos", httpError.error.message);
